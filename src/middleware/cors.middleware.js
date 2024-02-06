d0b246bde52f378a59dacf165f1f2f958fd6634a
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2024-02-06 19:21:14 +0545

    feat: enable cors for all routes

--- src/middleware/cors.middleware.js
+++ src/middleware/cors.middleware.js
@@ -1,10 +1,6 @@
 import cors from "cors";
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "http://localhost:5000",
-];
+const allowedOrigins = ["*"];
 
 export const corsHandler = cors({
   origin: (origin, callback) => {
